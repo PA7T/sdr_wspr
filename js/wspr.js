8a8a7d89300e8dd4c695ab338680fd51559acb5a
PA7T <PA7T@users.noreply.github.com>
2017-03-24 13:54:05 +0100

    proper GPLv3 reference

--- js/wspr.js
+++ js/wspr.js
@@ -1,11 +1,22 @@
 /*
- * Red Pitaya SDR WSPR Application
- *
- * Author: Clemens Heese / PA7T <pa7t@wsprlive.net>
- *
- * GPLv3 
- */
 
+Red Pitaya SDR WSPR Application - js web backbone
+Copyright (C) 2016-present  Clemens Heese / PA7T <pa7t@wsprlive.net>
+
+This program is free software: you can redistribute it and/or modify
+it under the terms of the GNU General Public License as published by
+the Free Software Foundation, either version 3 of the License, or
+(at your option) any later version.
+
+This program is distributed in the hope that it will be useful,
+but WITHOUT ANY WARRANTY; without even the implied warranty of
+MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+GNU General Public License for more details.
+
+You should have received a copy of the GNU General Public License
+along with this program.  If not, see <http://www.gnu.org/licenses/>.
+
+*/
 
 (function(WSPR, $, undefined) {
     
