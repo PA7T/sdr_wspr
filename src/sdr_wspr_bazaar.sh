0e13144aa9731d4162fd3af9106a3978e9c707f1
PA7T <pa7t@heesenet.de>
2017-03-30 09:02:22 +0000

    script added that zips the application for bazaar deployment

---
+++ src/sdr_wspr_bazaar.sh
@@ -0,0 +1,13 @@
+project=sdr_wspr
+
+cd /opt/redpitaya/www/apps/sdr_wspr/
+build_number=`git rev-list HEAD --count`
+revision=`git log -n1 --pretty=%h`
+
+make clean
+make INSTALL_DIR=/opt/redpitaya
+
+cd /opt/redpitaya/www/apps/
+sed -i "s/REVISION/$revision/; s/BUILD_NUMBER/$build_number/" $project/info/info.json
+sed -i "s/\(\"version\":\s\"\).*\"/\1$revision\"/g; s/\(\"revision\":\s\"\).*\"/\1$build_number\"/g" $project/info/info.json
+zip -r $project/bazaar/$project-0.97-$build_number.zip $project -x $project/src/**\* $project/.git/**\* $project/.gitignore $project/README.md $project/wspr-vars.sh $project/bazaar/**\* $project/.git/ $project/Makefile 
