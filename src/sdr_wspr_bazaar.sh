9a9a330bf92260ae6f3b6e053d02aa3123a86bd7
PA7T <PA7T@users.noreply.github.com>
2017-03-30 11:04:17 +0200

    removed old sed line

--- src/sdr_wspr_bazaar.sh
+++ src/sdr_wspr_bazaar.sh
@@ -8,6 +8,5 @@ make clean
 make INSTALL_DIR=/opt/redpitaya
 
 cd /opt/redpitaya/www/apps/
-sed -i "s/REVISION/$revision/; s/BUILD_NUMBER/$build_number/" $project/info/info.json
 sed -i "s/\(\"version\":\s\"\).*\"/\1$revision\"/g; s/\(\"revision\":\s\"\).*\"/\1$build_number\"/g" $project/info/info.json
 zip -r $project/bazaar/$project-0.97-$build_number.zip $project -x $project/src/**\* $project/.git/**\* $project/.gitignore $project/README.md $project/wspr-vars.sh $project/bazaar/**\* $project/.git/ $project/Makefile 
